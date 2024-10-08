a718b1906f65639ab94d395b2fded393c4d78f18
Javier <javier.gastonma@elorrieta-errekamari.com>
2024-10-08 16:50:23 +0200

    FEATURE Intento de meter datos a Student, no funciona, arreglar

--- app/src/main/java/com/example/entregapp/MainActivity.kt
+++ app/src/main/java/com/example/entregapp/MainActivity.kt
@@ -14,28 +14,68 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.second_activity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        /*
         val buttonLogin: Button = findViewById(R.id.buttonLogin)
         val buttonExit: Button = findViewById(R.id.buttonExit)
         val editText: EditText = findViewById(R.id.editTextUser)
         val editTextPassword: EditText = findViewById(R.id.editTextPassword2)
         var user: String = editText.text.toString()
         var password: String = editTextPassword.text.toString()
+
+         */
+
+        val button: Button = findViewById(R.id.button)
+        val editTextName: EditText = findViewById(R.id.editTextTextName)
+        val editTextFirstSurname: EditText = findViewById(R.id.editTextTextFirstSurname)
+        val editTextSecondSurname: EditText = findViewById(R.id.editTextTextSecondSurname)
+        val editTextProgamacion: EditText = findViewById(R.id.editTextTextProgramacion)
+        val editTextBases: EditText = findViewById(R.id.editTextTextBases)
+        val editTextSistemas: EditText = findViewById(R.id.editTextTextSistemas)
+        val editTextMarcas: EditText = findViewById(R.id.editTextTextMarcas)
+        val editTextEntornos: EditText = findViewById(R.id.editTextTextEntornos)
+
         val db = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java, "AppDatabase"
         ).allowMainThreadQueries().build()
+        /*
         buttonLogin.setOnClickListener {
-            var userLogin:String = editText.text.toString()
-            var passwordLogin:String = editTextPassword.text.toString()
+            var userLogin: String = editText.text.toString()
+            var passwordLogin: String = editTextPassword.text.toString()
             val user = User(userName = userLogin, password = passwordLogin)
             db.userDao().insertAll(user)
             Log.d("hola", "usuario introducido")
+        }
+
+         */
+
+        button.setOnClickListener {
+            var name: String = editTextName.text.toString()
+            var firstSurname: String = editTextFirstSurname.text.toString()
+            var secondSurname: String = editTextSecondSurname.text.toString()
+            var programacion: Int = editTextProgamacion.text.toString().toInt()
+            var bases: Int = editTextBases.text.toString().toInt()
+            var sistemas: Int = editTextSistemas.text.toString().toInt()
+            var marcas: Int = editTextMarcas.text.toString().toInt()
+            var entornos: Int = editTextEntornos.text.toString().toInt()
+
+            val student = Student(
+                studentname = name,
+                firstSurname = firstSurname,
+                secondSurname = secondSurname,
+                programacion = programacion,
+                basesdedatos = bases,
+                sistemas = sistemas,
+                marcas = marcas,
+                entornos = entornos
+            )
+            db.studentDao().insertAll(student)
         }
     }
 }
--- app/src/main/java/com/example/entregapp/Student.kt
+++ app/src/main/java/com/example/entregapp/Student.kt
@@ -10,5 +10,9 @@ class Student(
     @ColumnInfo(name = "studentName") val studentname: String,
     @ColumnInfo(name = "firstSurname") val firstSurname: String,
     @ColumnInfo(name = "secondSurname") val secondSurname: String,
-    @ColumnInfo(name = "Calification") val calification: Int
+    @ColumnInfo(name = "programacion") val programacion: Int,
+    @ColumnInfo(name = "basesdedatos") val basesdedatos: Int,
+    @ColumnInfo(name = "sistemas") val sistemas: Int,
+    @ColumnInfo(name = "marcas") val marcas: Int,
+    @ColumnInfo(name = "entornos") val entornos: Int,
 )
